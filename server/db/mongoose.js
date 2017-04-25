08cf188b9f82687bd5801b88e58ffc9bf8b3966b
shank136 <shashankchinmulgund@gmail.com>
2017-04-25 11:42:31 -0700

    updated mongoose.js file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
-var MONGODB_URI = 'mongodb://nodejsmongodb:Vishal27@ds119151.mlab.com:19151/udemynodejsmongodb';
-
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://nodejsmongodb:Vishal27@ds119151.mlab.com:19151/udemynodejsmongodb'
+}
+mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
 module.exports = {mongoose};
