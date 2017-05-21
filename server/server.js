b0250e6eb36a46b3009ca25e20b981f417630ada
shank136 <shashankchinmulgund@gmail.com>
2017-05-21 15:09:35 -0700

    Corrected DELETE route, all test cases passing

--- server/server.js
+++ server/server.js
@@ -142,7 +142,7 @@ app.post('/users/login', (req, res) => {
   });
 });
 
-app.delete('users/me/token', authenticate, (req, res) => {
+app.delete('/users/me/token', authenticate, (req, res) => {
   req.user.removeToken(req.token).then(() => {
     res.status(200).send();
   }, () => {
